16fc26f814a03b5c20fb03c4760940b74631b0f3
The DT <duongtuan30306@gmail.com>
2024-01-27 22:13:35 +0700

    update

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,7 @@
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
-    if (context.url.pathname.endsWith("/")) {
+    if (context.url.pathname.endsWith("/") && context.url.pathname !== "/") {
         context.url.protocol = "https://";
         context.url.pathname = context.url.pathname.slice(0, -1);
         return Response.redirect(context.url);

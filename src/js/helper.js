6086d0a808a586ab1df7824b90516425704d6107
The DT <duongtuan30306@gmail.com>
2024-01-29 00:18:09 +0700

    update

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,18 +1,3 @@
-for (let i of document.scripts) {
-    if (!i.src) {
-        continue;
-    }
-    i.onerror = function () {
-        let n_script = document.createElement("script");
-        for (let i of n_script.attributes) {
-            n_script.setAttribute(i.name, i.value);
-        }
-        n_script.src = `/proxy/script?u=${encodeURIComponent(i.src)}`;
-        i.parentNode.insertBefore(n_script, i);
-        i.remove();
-    }
-}
-
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag("js", new Date());

2366da3516cfe51e3889727d8341fb5df6b684f2
The DT <duongtuan30306@gmail.com>
2024-02-01 20:00:50 +0700

    update

--- src/js/helper.js
+++ src/js/helper.js
@@ -2,5 +2,5 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag("js", new Date());
 gtag("config", "G-YS5RWDRN7N", {
-    transport_url: location.origin + "/proxy/",
+    transport_url: location.origin + "/",
 });

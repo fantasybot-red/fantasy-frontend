a87f532e7d3948890000460567c0386c1c5ce5f1
The DT <duongtuan30306@gmail.com>
2024-02-03 15:00:37 +0700

    update

--- src/js/helper.js
+++ src/js/helper.js
@@ -272,9 +272,7 @@ import Hotjar from '@hotjar/browser';
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag("js", new Date());
-gtag("config", "G-YS5RWDRN7N", {
-    transport_url: location.origin + "/",
-});
+gtag("config", "G-YS5RWDRN7N");
 
 const siteId = 3849480;
 const hotjarVersion = 6;

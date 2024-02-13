5454224750ae6a574bfbee50a0e4e01221d42fe7
The DT <duongtuan30306@gmail.com>
2024-02-13 07:58:33 +0700

    update

--- src/pages/wp-admin/[path].js
+++ src/pages/wp-admin/[path].js
@@ -4,8 +4,10 @@ export function GET ({ request }) {
         "Content-Disposition": "attachment; filename=\"README\""
     };
     let body = `
-Try To become a Hacker :>>
-Then sory this page is not build by "Wordpress"
+Try To become a Hacker :>
+Hmm maybe this page is not build using "Wordpress"
+if you want to help me build this page please contact me through discord support server.
+or contact us if hacking success for bounty.
     `.trim();
     return new Response(body, { headers });
 };

68d3be21169c27fa2b7fa628f10bebb8fd3b7cb0
The DT <duongtuan30306@gmail.com>
2024-03-24 23:29:57 +0700

    update

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,7 @@
 import { defineMiddleware } from "astro:middleware";
 
+let fake_header = ["Express", "PHP", "ASP.NET", "Next.js", "JSF", "web2py", "Virgool", "Alpine", "Vanilla", "WP Engine", "PageFly", "ZendServer"];
+
 export const onRequest = defineMiddleware(async (context, next) => {
     if (context.url.pathname.endsWith("/") && context.url.pathname !== "/") {
         context.url.protocol = "https://";
@@ -8,5 +10,6 @@ export const onRequest = defineMiddleware(async (context, next) => {
     }
     let data = await next();
     data.headers.set("Status", `https://httpstatusdogs.com/img/${data.status}.jpg`);
+    data.headers.set("X-Powered-By", fake_header);
     return data;
 });
